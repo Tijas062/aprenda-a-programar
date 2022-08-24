fa051ae482af3170e58913ec8b5acd812a514bd9
Gabriel S <gabriel-c_crz@hotmail.com>
2022-08-24 20:35:38 -0300

    mensagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,4 +2,8 @@ let nome = "Gabriel"
 let email = "gabriel-c_crz@hotmail.com"
 let profissão = "agente callcenter"
 let idade = "19"
-let viagem = "japão"
+let viagem = "Japão"
+
+let mensagem = "Olá, "+ nome +" ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+viagem+". Confirma pra gente alguns dados? A sua idade é "+idade+" , você é "+profissão+" e o seu email é "+ email +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+console.log(mensagem)
